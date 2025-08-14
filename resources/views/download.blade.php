b2fabb18eb2689c1dfcf6e7ca822484d67fbc4fd
ByYags <jehpentester@gmail.com>
2025-08-15 00:30:57 +0800

    update

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -11,8 +11,12 @@
         <section id="download-content" class="section">
             <h2 class="section-title">Download the Game</h2>
             <div class="page-content">
-                <a href="https://mega.nz/file/fWpDRIAK#W4THSmERcq2KBHx9FKx2I5YxZcXSVLJg230b5I1WJEs" class="btn btn-primary">Download Now</a>
+                <a href="https://mega.nz/file/fWpDRIAK#W4THSmERcq2KBHx9FKx2I5YxZcXSVLJg230b5I1WJEs" class="btn btn-primary">Download (Mega)</a>
+            </div><br>
+            <div class="page-content">
+                <a href="https://www.mediafire.com/file/md4p1lmvkuod5oq/Ragnarok_ByYags_Client.zip/file" class="btn btn-primary">Download (Media Fire)</a>
             </div>
+
         </section>
     </div>
 @endsection
